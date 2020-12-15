03d28022c95573bf996292040566c5c126f7903a
Reaktor86 <mindlock861@gmail.com>
2020-12-15 18:31:46 +0300

    доделал карточку

--- starway-frontend-master/src/scripts/detail.js
+++ starway-frontend-master/src/scripts/detail.js
@@ -43,3 +43,39 @@ $(".details__color .color__circle-out:nth-child(4)").on("click", function (){
 $(".details__color .color__circle-out:nth-child(5)").on("click", function (){
     $(".details__slide img").attr("src", "images/stuff/bamboo-white.png");
 });
+
+// счётчик в корзине
+
+$(".added .added__minus").on("click", function (){
+    let num = $(".added .added_num").html();
+    num--;
+    if (num < 0) num = 0;
+    $(".added .added_num").html(num);
+});
+
+$(".added .added__plus").on("click", function (){
+    let num = $(".added .added_num").html();
+    num++;
+    $(".added .added_num").html(num);
+});
+
+// появление / закрытие попапа "добавлено в корзину"
+
+$(".details__buy").on("click", function (){
+    $(".added").css("display", "block");
+    $(".overlay").css("display", "block");
+    setTimeout(function (){
+        $(".added").css("bottom", "0");
+    }, 1);
+    //$(".added .added_num").html(0);
+});
+
+$(".added__continue, .added__close").on("click", function (){
+    setTimeout(function (){
+        $(".added").css("bottom", "-150%");
+    }, 1);
+    setTimeout(function (){
+        $(".added").css("display", "none");
+    }, 400);
+    $(".overlay").css("display", "none");
+});
